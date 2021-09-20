061a8189dbef0470ac18a013618dbcc4769acb43
JayabalArumugam <jayabal13497@gmail.com>
2021-09-20 23:43:02 +0530

    Initial commit

---
+++ src/AddTwoNo.java
@@ -0,0 +1,9 @@
+public class AddTwoNo {
+    public static void main(String[] args) {
+
+        int a=4, b=6;
+        int C;
+        C = a + b;
+        System.out.println("Sum of two numbers is: " + C);
+    }
+}
