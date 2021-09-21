2eae1be7a264597bf8c858a069a3b02cb354399f
JayabalArumugam <jayabal13497@gmail.com>
2021-09-21 13:32:01 +0530

    extra file added

--- src/Multiplication.java
+++ src/Multiplication.java
@@ -1,9 +1,9 @@
-public class AddTwoNo {
+public class Multiplication {
     public static void main(String[] args) {
 
-        int a=4, b=6;
+        int a = 4, b = 6;
         int C;
-        C = a + b;
+        C = a * b;
 
         System.out.println("Sum of two numbers is: " + C);
         System.out.println("Code passed");
