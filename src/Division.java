3bb4245bcbaaf7b22ef7b2dcb3dbd00b551dc51f
JayabalArumugam <jayabal13497@gmail.com>
2021-09-21 13:52:01 +0530

    divisioncode in branch

---
+++ src/Division.java
@@ -0,0 +1,11 @@
+public class Division {
+    public static void main(String[] args) {
+
+        int a = 12, b = 6;
+        int C;
+        C = a / b;
+
+        System.out.println("Sum of two numbers is: " + C);
+        System.out.println("Code passed");
+    }
+}
